0e75056d2012929f190e8c31750c144b4d6cc287
Arnaud Chen-yen-su <arnaud.chenyensu@gmail.com>
2012-03-25 17:37:51 +0400

    Sources

---
+++ src/i3/microblogging/distribue/Tweets.java
@@ -0,0 +1,75 @@
+package i3.microblogging.distribue;
+
+import com.techxplorer.testmeshms.MainActivity;
+import com.techxplorer.testmeshms.R;
+import com.techxplorer.testmeshms.R.id;
+import com.techxplorer.testmeshms.R.layout;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+
+public class Tweets extends ListActivity implements OnClickListener {
+
+	DBAdapter db = MainActivity.db;
+
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tweets);
+		((Button)findViewById(R.id.rafraichir)).setOnClickListener(this);
+		((Button)findViewById(R.id.send_tweet)).setOnClickListener(this);
+
+		DataBind();
+	}
+
+	public void DataBind() {
+		Cursor C = db.recupererTweets();
+		startManagingCursor(C);
+		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+				R.layout.list_item, C, new String[] {"sender", "content", "timestamp"},
+				new int[] {R.id.sender, R.id.content, R.id.timestamp}
+				);
+		setListAdapter(adapter);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see android.view.View.OnClickListener#onClick(android.view.View)
+	 */
+	public void onClick(View v) {
+		switch(v.getId()){
+		case R.id.send_tweet:
+			// show the send a message activity
+			Intent mSendActivityIntent = new Intent(this, com.techxplorer.testmeshms.SendActivity.class);
+			startActivity(mSendActivityIntent);
+			break;
+		case R.id.rafraichir:
+			DataBind(); //pour rafraichir la liste
+			break;
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onDestroy()
+	 */
+	public void onDestroy() {
+		db.close();
+		super.onDestroy();
+	}
+}
