84a480cf4fe38155f649cd0cc540fd4974464c52
Arnaud Chen-yen-su <arnaud.chenyensu@gmail.com>
2012-04-29 22:36:33 +0400

    Classes pour utiliser le service Rhizome de Serval

---
+++ src/org/servalproject/maps/rhizome/Rhizome.java
@@ -0,0 +1,225 @@
+/*
+ * Copyright (C) 2012 The Serval Project
+ *
+ * This file is part of the Serval Maps Software
+ *
+ * Serval Maps Software is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this source code; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.servalproject.maps.rhizome;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.servalproject.maps.utils.FileUtils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Environment;
+import android.text.TextUtils;
+import android.util.Log;
+import com.techxplorer.testmeshms.R;
+
+/**
+ * used to add a file to the Rhizome repository
+ */
+public class Rhizome {
+
+	/*
+	 * class level constants
+	 */
+	private static final String TAG = "Rhizome";
+
+	/**
+	 * add a file to the Rhizome repository
+	 * 
+	 * @param context a context object used to get access to system resources
+	 * @param filePath the full path to the file
+	 */
+	public static void addFile(Context context, String filePath) {
+
+		// check on the parameters
+		if(context == null) {
+			throw new IllegalArgumentException("the context parameter is required");
+		}
+
+		if(FileUtils.isFileReadable(filePath) == false) {
+			throw new IllegalArgumentException("unable to access the specified file");
+		}
+
+		// get access to the preferences
+		SharedPreferences mPreferences = context.getSharedPreferences("rhizome", Context.MODE_PRIVATE);
+
+		String mVersionName = new File(filePath).getName() + "-version";
+
+		Long mVersion = mPreferences.getLong(mVersionName, 0);
+
+		// increment the version
+		mVersion++;
+
+		// get the name identifier
+		String mName = context.getString(R.string.app_name);
+
+		// build the intent
+		Intent mIntent = new Intent("org.servalproject.rhizome.ADD_FILE");
+		mIntent.putExtra("path", filePath);
+		mIntent.putExtra("version", mVersion);
+		mIntent.putExtra("author", mName);
+		context.getApplicationContext().startService(mIntent);
+
+		// update version
+		Editor mEditor = mPreferences.edit();
+		mEditor.putLong(mVersionName, mVersion);
+		mEditor.commit();
+		Log.i(TAG, "File commited : " + filePath);
+	}
+
+	/**
+	 * check to see if a file is in Rhizome
+	 * 
+	 * @param fileNname the name of the file to look for
+	 * @return the full path to the file
+	 */
+	public static String checkForFile(Context context, String fileName) throws FileNotFoundException{
+
+		// check on the parameters
+		if(context == null) {
+			throw new IllegalArgumentException("the context parameter is required");
+		}
+
+		if(TextUtils.isEmpty(fileName)) {
+			throw new IllegalArgumentException("the file name parameter is required");
+		}
+
+		// check to see if the file is available
+		String FullPath = Rhizome.getFullPath(context, fileName);
+		if(FileUtils.isFileReadable(FullPath) == true) {
+			return FullPath;
+		} else {
+			throw new FileNotFoundException("Unable to read the specified file : " + FullPath);
+		}
+	}
+
+	/**
+	 * write to a file
+	 * 
+	 * @param context
+	 * @param path of the file
+	 * @param content to write
+	 */
+	public static void writeToFile(Context context, String path, String content) {
+		Log.i(TAG, "Write to file : " + path);
+		BufferedWriter writer = null;
+		try {
+			FileWriter fw = new FileWriter(Rhizome.checkForFile(context, path), true);
+			writer = new BufferedWriter(fw);
+			writer.append(content);
+			writer.newLine();
+			Log.i(TAG, "Write to file : " + content);
+		} catch (FileNotFoundException e1) {
+			Log.e(TAG, "FileNotFoundException", e1);
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			Log.e(TAG, "IOException", e1);
+			e1.printStackTrace();
+		}
+		//Close the BufferedWriter
+		try {
+			if (writer != null) {
+				writer.flush();
+				writer.close();
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+
+	/**
+	 * read a file
+	 * 
+	 * @param context
+	 * @param Filename the file to read
+	 */
+	public static void readFile(Context context, String Filename) {
+		Log.i(TAG, "Read file : " + Filename);
+		BufferedReader in = null;
+		try {
+			FileReader fr = new FileReader(Rhizome.checkForFile(context, Filename));
+			in = new BufferedReader(fr);
+			String line;
+			try {
+				while ((line = in.readLine()) != null) {
+					Log.i(TAG, "Read : " + line);
+				}
+			} catch (IOException e) {
+				Log.e(TAG, "IOException", e);
+			}
+			//Close the BufferedReader
+			try {
+				if(fr != null) {
+					in.close();
+				}
+			} catch (IOException e) {
+				Log.e(TAG, "IOException", e);
+			}
+		}
+		catch(FileNotFoundException e) {
+			Log.e(TAG, "FileNotFoundException", e);
+		}
+	}
+
+	/**
+	 * 
+	 * @param context
+	 * @param fileName
+	 * @return
+	 * @throws FileNotFoundException
+	 */
+	public static String getFullPath(Context context, String fileName) throws FileNotFoundException {
+		// check on the parameters
+		if(context == null) {
+			throw new IllegalArgumentException("the context parameter is required");
+		}
+
+		if(TextUtils.isEmpty(fileName)) {
+			throw new IllegalArgumentException("the file name parameter is required");
+		}
+
+		// get the rhizome path
+		String mRhizomePath = context.getString(R.string.system_path_rhizome_data);
+		try {
+			String mExternal = Environment.getExternalStorageDirectory().getCanonicalPath();
+			mRhizomePath = mExternal + mRhizomePath;
+		} catch (IOException e) {
+			Log.e(TAG, "unable to determine the full path to the Rhizome data store", e);
+			throw new FileNotFoundException("unable to determine the full path to the Rhizome data store");
+		}
+
+		// check on the rhizome path
+		if(FileUtils.isDirectoryWritable(mRhizomePath) == false) {
+			Log.e(TAG, "unable to access the rhizome directory: " + mRhizomePath);
+			throw new FileNotFoundException("unable to access the rhizome directory: " + mRhizomePath);
+		}
+
+		return(mRhizomePath + fileName);
+	}
+}
---
+++ src/org/servalproject/maps/rhizome/RhizomeBroadcastReceiver.java
@@ -0,0 +1,159 @@
+///*
+// * Copyright (C) 2012 The Serval Project
+// *
+// * This file is part of the Serval Maps Software
+// *
+// * Serval Maps Software is free software; you can redistribute it and/or modify
+// * it under the terms of the GNU General Public License as published by
+// * the Free Software Foundation; either version 3 of the License, or
+// * (at your option) any later version.
+// *
+// * This source code is distributed in the hope that it will be useful,
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// * GNU General Public License for more details.
+// *
+// * You should have received a copy of the GNU General Public License
+// * along with this source code; if not, write to the Free Software
+// * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// */
+//
+//package org.servalproject.maps.rhizome;
+//
+//import java.io.File;
+//import java.io.IOException;
+//import java.util.concurrent.ExecutorService;
+//
+//import org.servalproject.maps.utils.FileUtils;
+//
+//import android.R;
+//import android.content.BroadcastReceiver;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.os.Environment;
+//import android.util.Log;
+//
+///**
+// * receives the broadcasts from Rhizome about new files being available
+// */
+//public class RhizomeBroadcastReceiver extends BroadcastReceiver {
+//
+//	/*
+//	 * private class level constants
+//	 */
+//	private final boolean V_LOG = true;
+//	private final String TAG = "RhizomeBroadcastReceiver";
+//
+//	/*
+//	 * private class level variables
+//	 */
+//	ExecutorService executor;
+//
+//	/**
+//	 * constructor a new instance of this receiver providing it an executor service
+//	 * used to manage the data import threads
+//	 * 
+//	 * @param executor used to manage the execution of data import threads
+//	 * @throws IllegalArgumentException if executor is null
+//	 */
+//	public RhizomeBroadcastReceiver(ExecutorService executor) {
+//
+//		if(executor == null) {
+//			throw new IllegalArgumentException("the executor parameter is required");
+//		}
+//
+//		this.executor = executor;
+//
+//	}
+//
+//	/*
+//	 * (non-Javadoc)
+//	 * @see android.content.BroadcastReceiver#onReceive(android.content.Context, android.content.Intent)
+//	 */
+//	@Override
+//	public void onReceive(Context context, Intent intent) {
+//
+//		Bundle mBundle = intent.getExtras();
+//
+//		if(intent.getAction().equals("org.servalproject.rhizome.RECIEVE_FILE") == false) {
+//			Log.e(TAG, "called with an intent with an unexepcted intent action");
+//			return;
+//		}
+//
+//		// see if the file is one we want to work with
+//		String mFilePath = mBundle.getString("path");
+//
+//		if(mFilePath == null) {
+//			Log.e(TAG, "called with an intent missing the 'path' extra");
+//		}
+//
+//		// check to see if the file path is to our own file
+//		ServalMaps mServalMaps = (ServalMaps) context.getApplicationContext();
+//		String mFileName = new File(mFilePath).getName();
+//		String[] mFileParts = mFileName.split("-");
+//
+//		String mPhoneNumber = mServalMaps.getPhoneNumber();
+//		mPhoneNumber = mPhoneNumber.replace(" ", "");
+//		mPhoneNumber = mPhoneNumber.replace("-", "");
+//
+//		if(mFileParts[0].equals(mServalMaps.getPhoneNumber()) == true) { 
+//			// this doesn't look like one of our own binary files
+//			return;
+//		}
+//
+//		// is it one of our images?
+//		if(mFileName.startsWith(MediaUtils.PHOTO_FILE_PREFIX) && mFileName.endsWith(".jpg")) {
+//			// this is a serval maps photo
+//			try {
+//				FileUtils.copyFileToDir(mFilePath, MediaUtils.getMediaStore());
+//				Log.d(TAG, MediaUtils.getMediaStore());
+//			} catch (IOException e) {
+//				Log.e(TAG, "unable to copy file", e);
+//				return;
+//			}
+//		}
+//
+//		// get the binary data directory
+//		String mDataPath = Environment.getExternalStorageDirectory().getPath();
+//		mDataPath += context.getString(R.string.system_path_binary_data);
+//
+//		if(mFilePath.endsWith(BinaryFileContract.LOCATION_EXT) == true) {
+//			// this is a binary location file
+//
+//			// copy and process the file
+//			try {
+//
+//				String mDataFile = FileUtils.copyFileToDirWithTmpName(mFilePath, mDataPath);				
+//				LocationReadWorker mWorker = new LocationReadWorker(context, mDataFile);
+//				executor.submit(mWorker);
+//
+//			} catch (IOException e) {
+//				Log.e(TAG, "unable to copy file", e);
+//				return;
+//			}
+//
+//		} else if(mFilePath.endsWith(BinaryFileContract.POI_EXT) == true) {
+//			// this is a binary POI file
+//
+//			// copy and process the file
+//			try {
+//
+//				String mDataFile = FileUtils.copyFileToDirWithTmpName(mFilePath, mDataPath);
+//				PointsOfInterestWorker mWorker = new PointsOfInterestWorker(context, mDataFile);
+//				executor.submit(mWorker);
+//
+//			} catch (IOException e) {
+//				Log.e(TAG, "unable to copy file", e);
+//				return;
+//			}
+//		}
+//
+//		if(V_LOG) {
+//			Log.v(TAG, "received intent with action: " + intent.getAction());
+//			Log.v(TAG, "file name: " + mBundle.getString("path"));
+//			Log.v(TAG, "version: " + Long.toString(mBundle.getLong("version")));
+//			Log.v(TAG, "name: " + mBundle.getString("name"));
+//		}
+//	}
+//}
