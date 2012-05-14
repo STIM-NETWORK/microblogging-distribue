2298eff035705ff48d13010228a0f279ec7a221b
Arnaud Chen-yen-su <arnaud.chenyensu@gmail.com>
2012-05-14 12:09:07 +0400

    Update de la licence

--- src/org/servalproject/maps/rhizome/RhizomeBroadcastReceiver.java
+++ src/org/servalproject/maps/rhizome/RhizomeBroadcastReceiver.java
@@ -1,5 +1,8 @@
 /*
  * Copyright (C) 2012 ESIROI. All rights reserved.
+ * 
+ * This file is part of the Serval Maps Software
+ * 
  * StimTweets is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
